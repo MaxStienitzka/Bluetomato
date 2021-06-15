8a5398c464e31f8eb0c0f3b0df6072b0155c0f2d
MaxStienitzka <maximilian.stienitzka03@htl-salzburg.ac.at>
2021-06-15 19:22:32 +0200

    final beta 3.0, Guide gotta be cleaned up

--- Assets/new_UIScripts/GuideScript.cs
+++ Assets/new_UIScripts/GuideScript.cs
@@ -8,6 +8,11 @@ public class GuideScript : MonoBehaviour
 {
     public GameObject auswahl;
     public GameObject guideOne;
+    public GameObject guideTwo;
+    public GameObject guideThree;
+    public GameObject guideFour;
+    public GameObject guideFive;
+    public GameObject guideSix;
 
     public void GuideOne()
     {
@@ -15,9 +20,45 @@ public class GuideScript : MonoBehaviour
         auswahl.SetActive(false);
     }
 
+    public void GuideTwo()
+    {
+        guideTwo.SetActive(true);
+        auswahl.SetActive(false);
+    }
+
+    public void GuideThree()
+    {
+        guideThree.SetActive(true);
+        auswahl.SetActive(false);
+    }
+
+    public void GuideFour()
+    {
+        guideFour.SetActive(true);
+        auswahl.SetActive(false);
+    }
+
+    public void GuideFive()
+    {
+        guideFive.SetActive(true);
+        auswahl.SetActive(false);
+    }
+
+    public void GuideSix()
+    {
+        guideSix.SetActive(true);
+        auswahl.SetActive(false);
+    }
+
     public void Reset()
     {
         auswahl.SetActive(true);
         guideOne.SetActive(false);
+        guideTwo.SetActive(false);
+        guideThree.SetActive(false);
+        guideThree.SetActive(false);
+        guideFour.SetActive(false);
+        guideFive.SetActive(false);
+        guideSix.SetActive(false);
     }
 }
